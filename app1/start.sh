e2f5a3ff7da198c863024b78032386e67e375cec
Christian <joppi94@gmail.com>
2017-10-30 15:38:32 +0100

    fix admin static file

--- app1/start.sh
+++ app1/start.sh
@@ -2,14 +2,17 @@
 
 cd helloworld/
 
-# Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
+
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
 
 #Start Unicorn Process
 echo Starting Unicorn.
