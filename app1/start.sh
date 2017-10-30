f2ec9841d3ab49b4387b628fc1e864e6513bddc6
Christian <joppi94@gmail.com>
2017-10-30 14:38:24 +0100

    auto migration db

--- app1/start.sh
+++ app1/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py makemigrations
+python manage.py migrate
+
+
 #Start Unicorn Process
 echo Starting Unicorn.
 
