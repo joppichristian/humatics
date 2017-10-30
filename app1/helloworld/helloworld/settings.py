0d2b95d05548e1affd44f10e6d15f1c9bc3c6b25
Christian <joppi94@gmail.com>
2017-10-30 15:49:25 +0100

    fix static file

--- app1/helloworld/helloworld/settings.py
+++ app1/helloworld/helloworld/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = '/usr/src/app/helloworld/static'
+STATIC_ROOT = '/home/django/django_project/django_project/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
