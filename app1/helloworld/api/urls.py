58922bb8227e343773fff30110f06d1d53e15522
Christian <joppi94@gmail.com>
2017-10-30 16:29:25 +0100

    FIXED STATIC FILES
    TODO: UNICORN URLS STATIC

--- app1/helloworld/api/urls.py
+++ app1/helloworld/api/urls.py
@@ -8,6 +8,7 @@ from .views import ImageView,UserViewSet
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'', UserViewSet)
 router.register(r'images',ImageView)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- app1/helloworld/helloworld/urls.py
+++ app1/helloworld/helloworld/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('api.urls')),
     url(r'^api-token-auth/', obtain_auth_token),
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#TODO: Controllare Unicorn file statici
