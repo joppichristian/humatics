766080b06a1daf96adcc2dd3b2effdfc0b407c8d
Christian <joppi94@gmail.com>
2017-10-30 14:45:24 +0100

    fix auto migration db

--- app1/start.sh
+++ app1/start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
+cd helloworld/
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-cd helloworld/
 python manage.py makemigrations
 python manage.py migrate
-cd ..
+cd ../
 
 #Start Unicorn Process
 echo Starting Unicorn.
